c184e035d9f4028f73d2e0c95244f2018cc4b1e9
SauloPerceval <44772599+SauloPerceval@users.noreply.github.com>
2019-11-30 11:38:03 -0300

    Fix mutation and cross and return nets with result on turnament

--- neuroevolution/evolution_utils.py
+++ neuroevolution/evolution_utils.py
@@ -3,16 +3,19 @@ import random
 
 from neuroevolution.neural_network import NeuralNetwork
 
+mutation_percent = 0.5
 
-def cross(parents_nets):
-    num_childs = 5
+
+def cross(parents_nets, num_childs):
     childs = list()
 
     for i in range(num_childs):
-        alpha = numpy.clip(numpy.random.normal(loc=0.5, scale=0.1), a_min=0, a_max=1)
-
         first_parent = random.choice(parents_nets)
         second_parent = random.choice([net for net in parents_nets if net != first_parent])
+
+        alpha = numpy.clip(
+            numpy.random.normal(loc=0.5, scale=0.1, size=first_parent.weights_to_1D_array().size), a_min=0, a_max=1
+        )
 
         child_weights = numpy.add(first_parent.weights_to_1D_array()*alpha,
                                   second_parent.weights_to_1D_array()*(1-alpha))
@@ -22,32 +25,32 @@ def cross(parents_nets):
 
 
 def mutate(childs_nets):
-    mutant_childs = list()
-    for child_net in childs_nets:
-        child_weights = child_net.weights_to_1D_array()
-        mutation_pos = random.randint(0, len(child_weights)-1)
-        # child_weights[mutation_pos] = numpy.random.normal(loc=0, scale=0.1)
+    childs_to_mutate = random.sample(childs_nets, k=round(len(childs_nets)*mutation_percent))
+    [childs_nets.remove(child_to_mutate) for child_to_mutate in childs_to_mutate]
+    for child_net_to_mutate in childs_to_mutate:
+        child_weights = child_net_to_mutate.weights_to_1D_array()
 
-        mutant_childs.append(NeuralNetwork(child_weights+0.2*numpy.random.normal(loc=0, scale=0.1)))
+        childs_nets.append(
+            NeuralNetwork(child_weights+numpy.random.normal(loc=0, scale=0.1, size=child_weights.size))
+        )
 
-    return mutant_childs
+    return childs_nets
 
 
-def tournament(results_n_nets):
-    num_next_gen_parents = 5
-    next_gen_parents_nets = list()
+def tournament(results_n_nets, num_next_gen_parents):
+    next_gen_parents_w_result = list()
 
     for i in range(num_next_gen_parents):
         first_defiant = random.choice(results_n_nets)
         second_defiant = random.choice([result_net for result_net in results_n_nets if result_net != first_defiant])
         # Winner is determine by the first element on the result of the net (the fitness)
-        winner = max(first_defiant, second_defiant, key=lambda defiant: defiant[0][0])
+        winner = min(first_defiant, second_defiant, key=lambda defiant: defiant[0][2])
 
         results_n_nets.remove(winner)
         # Add the winner network to the next gen parents
-        next_gen_parents_nets.append(winner[1])
+        next_gen_parents_w_result.append(winner)
 
-    return next_gen_parents_nets
+    return next_gen_parents_w_result
 
 
 def save_generation(generation_population):
