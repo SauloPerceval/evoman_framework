bf984ab51bdf1f6143c5c35ad49fcc4dbbf8f722
SauloPerceval <44772599+SauloPerceval@users.noreply.github.com>
2019-11-30 11:40:36 -0300

    change population size to 40 individuals and play 5 games at once

--- run_neuroplayer.py
+++ run_neuroplayer.py
@@ -25,11 +25,14 @@ def play_net(net):
 
 
 def play_generation(nets):
+    bash_size = 5
+    generation_result = list()
     start_time = time.time()
-    with Pool(5) as p:
-        generation_result = list(p.map(play_net, nets[:5]))
-    with Pool(5) as p:
-        generation_result.extend(list(p.map(play_net, nets[5:])))
+    last_idx = 0
+    for idx in range(bash_size, len(nets)+1, bash_size):
+        with Pool(bash_size) as p:
+            generation_result.extend(list(p.map(play_net, nets[last_idx:idx])))
+            last_idx = idx
     finish_time = time.time()
     print(finish_time - start_time)
 
@@ -39,21 +42,37 @@ def play_generation(nets):
 
 if __name__ == '__main__':
     num_generation = 20
+    initial_population_num = 20
 
-    parent_nets = list(NeuralNetwork() for i in range(5))
+    parent_nets = list(NeuralNetwork() for i in range(initial_population_num))
 
     # parent_nets = load_generation()
 
+    # first play
+    print(f"\ngen 0")
+    child_nets = cross(parent_nets, num_childs=initial_population_num)
+
+    child_nets = mutate(child_nets)
+
+    save_generation(parent_nets + child_nets)
+
+    generation_result = play_generation(parent_nets + child_nets)
+
+    print([individual_result[0][0] for individual_result in generation_result])
+
+    parent_nets_w_results = tournament(generation_result, num_next_gen_parents=initial_population_num)
+
     for i in range(num_generation):
-        print(f"gen {i}")
-        child_nets = cross(parent_nets)
+        print(f"\ngen {i+1}")
+        child_nets = cross(parent_nets, num_childs=initial_population_num)
 
         child_nets = mutate(child_nets)
 
         save_generation(parent_nets + child_nets)
 
-        generation_result = play_generation(parent_nets + child_nets)
+        child_results = play_generation(child_nets)
+        generation_result = parent_nets_w_results + child_results
 
-        print([individual_result[0][0] for individual_result in generation_result])
+        print([individual_result[0][2] for individual_result in generation_result])
 
-        parent_nets = tournament(generation_result)
+        parent_nets_w_results = tournament(generation_result, num_next_gen_parents=initial_population_num)
